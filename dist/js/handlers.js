4ab84fd7d8d825808025f5836a6fb5d42f9b2ece
Evgenii <evtatdevelop@gmail.com>
2020-09-28 15:53:52 +0700

    drag of form added

--- dist/js/handlers.js
+++ dist/js/handlers.js
@@ -3,6 +3,7 @@ const ESC_KEYCODE = 27
 let $apps = document.querySelector('#applications')
 let $form = document.querySelector('#appForm')
 let closer = $form.querySelector('.closer')
+let formHandler = $form.querySelector('.mover')
 
 // Event of the opening of form
 $apps.addEventListener('click', evt => {
@@ -19,6 +20,40 @@ const closeForm = () => {
     })
 }
 
+formHandler.onmousedown = evt => {
+
+    $form.classList.add('drug')
+
+    var coords = getCoords($form)
+    var shiftX = evt.pageX - coords.left
+    var shiftY = evt.pageY - coords.top
+
+    function moveAt(evt) {
+        $form.style.left = evt.pageX - shiftX + 'px'
+        $form.style.top = evt.pageY - shiftY + 'px'
+    }
+  
+    document.onmousemove = evt => moveAt(evt)
+  
+    $form.onmouseup = () => {
+      document.onmousemove = null
+      $form.onmouseup = null
+      $form.classList.remove('drug')
+    }
+  
+  }
+  
+function getCoords(elem) { 
+    let box = elem.getBoundingClientRect();
+    return {
+        // TODO: !!! move align to js from css
+        // form.scss !!! 350 , 150 - margin-top, margin-left from 50% position #appForm !!! 
+        top: box.top + pageYOffset + 350,
+        left: box.left + pageXOffset + 150
+    }
+}
+
+
 // Event for the closing of the form
 closer.addEventListener('click', closeForm)
 closer.addEventListener('keydown', evt => {
@@ -26,4 +61,3 @@ closer.addEventListener('keydown', evt => {
         closeForm()
     }
 })
-
